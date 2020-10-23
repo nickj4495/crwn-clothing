d8f9da1b7ddd2e08a0451fa0114c9ca77b9b8d6c
nickj4495 <nickj4495@gmail.com>
2020-10-22 18:11:12 -0700

    created a shop collection selector for converting collections object to an array, making map array usable in collections overview component

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -3,15 +3,20 @@ import { connect } from "react-redux";
 
 import CollectionItem from "../../components/collection-item/collection-item.component";
 
-import { selectCollection } from "../../redux/shop/shop.selector";
+import { selectCollection } from "../../redux/shop/shop.selectors";
 
 import "./collection.component.styles.scss";
 
 const CollectionPage = ({ collection }) => {
-  console.log(collection);
+  const { title, items } = collection;
   return (
     <div className="collection-page">
-      <h2>CATEGORY PAGE</h2>
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   );
 };
